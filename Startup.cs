ff56b565b1a03932a910a18e2b77b72d9afe0281
ChinChuanKuo <chinchuankuo@flytech-vdi-4.flytech.com.tw>
2020-12-16 14:41:29 +0800

    module better

--- Startup.cs
+++ Startup.cs
@@ -32,9 +32,9 @@ namespace signformApi
             {
                 options.AddDefaultPolicy(builder => builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
                 options.AddPolicy("Signin",
-                    builder => builder.WithOrigins("http://221.222.222.181:6350/").AllowAnyHeader().WithMethods("POST"));
+                    builder => builder.WithOrigins("http://221.222.222.181:6350").AllowAnyHeader().AllowAnyMethod());
                 options.AddPolicy("Signout",
-                    builder => builder.WithOrigins("http://221.222.222.181:6350/").AllowAnyHeader().WithMethods("POST"));
+                    builder => builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
             });
             services.AddControllers();
         }
