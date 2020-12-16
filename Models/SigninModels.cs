e121a08d5ad93bcee6fca86c6cef2363ed078d3b
ChinChuanKuo <chinchuankuo@flytech-vdi-4.flytech.com.tw>
2020-12-16 11:51:17 +0800

    module better

--- Models/SigninModels.cs
+++ Models/SigninModels.cs
@@ -11,12 +11,12 @@ namespace signformApi.Models
             database database = new database();
             DataTable mainRows = new DataTable();
             List<dbparam> dbparamlist = new List<dbparam>();
-            dbparamlist.Add(new dbparam("@iid", sItemData.items["iid"]));
+            dbparamlist.Add(new dbparam("@iid", int.Parse(sItemData.items["iid"].ToString().TrimEnd())));
             mainRows = database.checkSelectSql("mssql", "sysstring", "exec web.searchflowform @iid;", dbparamlist);
             switch (mainRows.Rows.Count)
             {
                 case 0:
-                    return new statusModels() { status = sItemData.items["iid"].ToString().TrimEnd() };
+                    return new statusModels() { status = "nodata" };
             }
             dbparamlist.Add(new dbparam("@name", mainRows.Rows[0]["nm"].ToString().TrimEnd()));
             dbparamlist.Add(new dbparam("@note", ""));
